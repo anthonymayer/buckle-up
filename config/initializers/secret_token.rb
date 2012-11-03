9839341b06fe4469a25a2e5e2dfc3b6a88be50a5
Anthony Mayer <adwmayer@gmail.com>
2012-11-03 09:36:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buckleup::Application.config.secret_token = 'd136186c1fbc2516035159940a574d282050c51482cd4f374ec1b6cb05b30930b2155e76162eae2e1785961ecbc0333ea94c5aec398571809b9db4bfd249ac2c'
